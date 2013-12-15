06591fd1b8253a59ac990238eb9ec947fbccbe2d
Duncan Burggraf <dcburggraf@gmail.com>
2013-12-14 16:04:53 -0800

    added docstrings

--- src/Body.py
+++ src/Body.py
@@ -7,16 +7,21 @@ class Body:
         pass
     
     def p(self):
-        return 'body'
+        """Returns the name of the type of body this is."""
+        return 'Body'
     
     def get_color(self, point):
+        """Returns color of body at given point."""
         pass
     
     def normal(self, point):
+        """Returns normal vector of body at given point."""
         pass
     
     def reflectivity(self, point):
+        """Returns percentage of brightness due to specular reflection."""
         pass
     
     def intersection(self, ray):
+        """Returns distance at which ray intersects body."""
         pass
