cbc0ce8a31c758993e97f0a80d4484001b6900f9
Duncan Burggraf <dcburggraf@gmail.com>
2013-12-21 12:00:43 -0800

    profiling Interface-test

--- test/Interface-test.py
+++ test/Interface-test.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python2
 
-from raytrace.src.Vector import Vector
-from raytrace.src.Ray import Ray
-from raytrace.src.Body import Body
-from raytrace.src.Color import Color
-from raytrace.src.Interface import Interface
+from py3D import Vector, Ray, Body, Color
+from pytrace import Interface
+import cProfile
 
 class bodystub(Body):
     
@@ -43,27 +41,28 @@ for origins in ray_origins:
         rays.append(Ray(origins.dup().delta(0.1),directions.dup()))
 
 i = Interface()
+def run():
 
-for r in rays:
-    i.reset()
-    for b in bodies:
-        d = b.intersection(r)
-        if d > 0.0:
-            i.hit(b,d)
-    i.register_hit(r)
-    if i._body == None:
-        print "interface didn't register a hit"
-    else:
-        if i._distance < 0.0:
-            print "interface registered hit at negative distance"
-        if i._distance > 0.1:
-            print "interface didn't catch closest hit"
-        if  i._body.p() != 'bodystub':
-            print "interface didn't catch body of correct type"
-        if not i._normal == Vector(0.0,0.0,1.0):
-            print "interface caught wrong surface normal."
-        if not i._color == Color(0.5,0.5,0.5):
-            print "interface caught wrong surface color."
+    for r in rays:
+        i.reset()
+        for b in bodies:
+            d = b.intersection(r)
+            if d > 0.0:
+                i.hit(b,d)
+        i.register_hit(r)
+        if i._body == None:
+            print "interface didn't register a hit"
+        else:
+            if i._distance < 0.0:
+                print "interface registered hit at negative distance"
+            if i._distance > 0.1:
+                print "interface didn't catch closest hit"
+            if  i._body.p() != 'bodystub':
+                print "interface didn't catch body of correct type"
+            if not i._normal == Vector(0.0,0.0,1.0):
+                print "interface caught wrong surface normal."
+            if not i._color == Color(0.5,0.5,0.5):
+                print "interface caught wrong surface color."
 
-
+cProfile.run('run()')
 
