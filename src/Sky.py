19588858571899bb44057589e10af42c792cf26e
Duncan Burggraf <dcburggraf@gmail.com>
2013-12-24 11:21:14 -0800

    Added method to change brightness of sun.

--- src/Sky.py
+++ src/Sky.py
@@ -1,5 +1,7 @@
 from py3D import Vector, Color
 
+_default_exponent = 20.0
+
 class Sky:
     """Class that describes the sky."""
     
@@ -7,10 +9,18 @@ class Sky:
             color = Color(0.2,0.2,0.9)):
         self.light = light.norm()
         self.color = color
+        self._exp = _default_exponent
     
-    def get_color(self, ray):
+    def set_brightness(self, exp):
+        """Sets brightness of sun: low is brighter."""
+        self._exp = exp
+        return self
+    
+    def get_color(self, ray, exp = None):
+        if exp == None:
+            exp = _default_exponent
         d = max(ray.d.dot(self.light), 0)
-        return self.color.dup().gamma(1 - d ** 20)
+        return self.color.dup().gamma(1 - d ** self._exp)
     
     def get_light(self):
         return self.light.dup()
